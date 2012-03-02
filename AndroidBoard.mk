3d3cb481d92c10a8d06eebeed5a4902b76c10063
karim bourahmani <karimx.bourahmani@intel.com>
2012-03-02 08:51:10 +0100

    Factorization of vendor/intel/common
    BZ: 23278
    
    Change-Id: I99d287759d7f0458a9f056acb2dacf669e92a91e
    Signed-off-by: karim bourahmani <karimx.bourahmani@intel.com>
    Reviewed-on: http://android.intel.com:8080/37368
    Reviewed-by: buildbot <buildbot@intel.com>
    Reviewed-by: Desurmont, DavidX <davidx.desurmont@intel.com>
    Tested-by: Letessier, ChristopheX <christophex.letessier@intel.com>
    Tested-by: Monnier, OlivierX <olivierx.monnier@intel.com>

---
+++ AndroidBoard.mk
@@ -0,0 +1,82 @@
+# make file for common
+#
+
+LOCAL_PATH := $(TOP)/vendor/intel/common
+PERMISSIONS_PATH := $(TOP)/frameworks/base/data/etc
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := mrst_keypad.kcm
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_KEY_CHAR_MAP)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := mrst_keypad.kl
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := mrst_keypad.kl
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_PATH := $(TARGET_OUT_KEYLAYOUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := KEYPAD.kl
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := KEYPAD.kl
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_PATH := $(TARGET_OUT_KEYLAYOUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := gpio-keys.kl
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := gpio-keys.kl
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_PATH := $(TARGET_OUT_KEYLAYOUT)
+include $(BUILD_PREBUILT)
+
+# USB port turn around and initialization
+PRODUCT_INIT_RC_FILES += vendor/intel/common/init.mfld.usb.rc
+
+# OOM Killer tweaks
+PRODUCT_INIT_RC_FILES += vendor/intel/common/init.mfld.oom.rc
+
+# The kernel and kernel_modules were copied to PRODUCT_OUT by build.sh.
+# The kernel needs to be renamed to 'kernel' for the build.
+PRODUCT_COPY_FILES += \
+        $(PRODUCT_OUT)/bzImage:kernel
+
+# board specific files
+PRODUCT_COPY_FILES += \
+        $(LOCAL_PATH)/android.conf:system/etc/dhcpcd/android.conf \
+        $(LOCAL_PATH)/mxt224_touchscreen_0.idc:system/usr/idc/mxt224_touchscreen_0.idc \
+        $(LOCAL_PATH)/activate_trace_modem:system/bin/activate_trace_modem \
+        $(LOCAL_PATH)/configure_trace_modem:system/bin/configure_trace_modem \
+        $(LOCAL_PATH)/usbswitch_modem:system/bin/usbswitch_modem \
+        $(LOCAL_PATH)/usbswitch_ape:system/bin/usbswitch_ape \
+        $(LOCAL_PATH)/usbswitch_status:system/bin/usbswitch_status \
+        $(LOCAL_PATH)/toggle_pin_on1:system/bin/toggle_pin_on1
+
+# platform common files
+PRODUCT_COPY_FILES += \
+        $(LOCAL_PATH)/init.rc:init.rc \
+        $(LOCAL_PATH)/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
+        $(LOCAL_PATH)/init.nfs.rc:root/init.nfs.rc \
+        $(LOCAL_PATH)/init.sdcard1.rc:root/init.sdcard1.rc \
+        $(LOCAL_PATH)/init.sdcard2.rc:root/init.sdcard2.rc \
+        frameworks/base/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
+        $(PERMISSIONS_PATH)/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+        $(PERMISSIONS_PATH)/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+        $(PERMISSIONS_PATH)/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+        $(PERMISSIONS_PATH)/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+        $(PERMISSIONS_PATH)/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+        $(PERMISSIONS_PATH)/android.hardware.sensor.barometer.xml:system/etc/permissions/android.hardware.sensor.barometer.xml \
+        $(PERMISSIONS_PATH)/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
+        $(PERMISSIONS_PATH)/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+        $(PERMISSIONS_PATH)/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+        $(PERMISSIONS_PATH)/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+        $(PERMISSIONS_PATH)/android.hardware.touchscreen.xml:system/etc/permissions/android.hardware.touchscreen.xml \
+        $(PERMISSIONS_PATH)/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml \
+        $(PERMISSIONS_PATH)/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
+        $(PERMISSIONS_PATH)/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+        $(PERMISSIONS_PATH)/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+        $(PERMISSIONS_PATH)/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml
+
---
+++ BoardConfig.mk
@@ -0,0 +1,69 @@
+#
+# config.mk for mrst
+#
+
+TARGET_ARCH := x86
+TARGET_ARCH_VARIANT := x86-atom
+TARGET_BOARD_PLATFORM := mrst
+TARGET_BOOTIMAGE_USE_EXT2 := true
+TARGET_CPU_ABI := x86
+TARGET_CPU_SMP := true
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+TARGET_NO_RECOVERY := false
+
+TARGET_PRELINK_MODULE := false
+TARGET_PROVIDES_INIT_RC := true
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_RIL_DISABLE_STATUS_POLLING := true
+
+#Platform
+DISABLE_DEXPREOPT := true
+
+ADDITIONAL_DEFAULT_PROPERTIES += wifi.interface=wlan0:0
+
+ADDITIONAL_DEFAULT_PROPERTIES += gsm.net.interface=rmnet0
+#                                 gsm.net.interface=gtm0
+
+# Enabling logs into file system by default
+ADDITIONAL_DEFAULT_PROPERTIES += persist.service.apklogfs.enable=1 \
+                                 persist.service.crashlog.enable=1
+
+ifeq ($(PRODUCT_MANUFACTURER),intel)
+ifeq ($(TARGET_BUILD_VARIANT),eng)
+ADDITIONAL_DEFAULT_PROPERTIES += persist.service.profile.enable=1
+endif
+endif
+
+#ifeq ($(TARGET_BUILD_VARIANT),eng)
+#ADDITIONAL_DEFAULT_PROPERTIES += persist.core.enabled=1
+#endif
+
+ifeq ($(TARGET_RIL_DISABLE_STATUS_POLLING),true)
+ADDITIONAL_BUILD_PROPERTIES += ro.ril.status.polling.enable=0
+endif
+
+#Intel recovery images and boot images are different from android images.
+INTEL_BOOTIMG := true
+
+ifeq ($(BOARD_BOOTMEDIA),)
+BOARD_BOOTMEDIA := sdcard
+endif
+
+BOOT_TARBALL_FORMAT := gz
+SYSTEM_TARBALL_FORMAT := gz
+
+# Required for the size calculations in definitions.mk. Since
+# definitions.mk assume a nand... a bit of space will be wasted
+BOARD_FLASH_BLOCK_SIZE := 2048
+BUILD_WITH_FULL_STAGEFRIGHT := true
+
+
+# Set property of maximal runtime heap size to 64MB for intel's mfld board.
+# If not set, one app can only use at most 16MB for runtime heap, which is
+# too small and sometimes would cause out-of-memory error.
+ADDITIONAL_BUILD_PROPERTIES += dalvik.vm.heapsize=64m
+
+# Security
+BUILD_WITH_CHAABI_SUPPORT := true
+BUILD_WITH_WATCHDOG_DAEMON_SUPPORT := true
