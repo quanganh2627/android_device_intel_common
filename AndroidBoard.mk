b6774f7d4bfd642d36474ac3eb6ab7e50c27c95c
Zhang Dongxing <dongxing.zhang@intel.com>
2012-05-07 14:20:09 +0800

    Wuwatch and VTune: upgrade for sepdk and powerdk directory change
    BZ: 34965
    
    Change-Id: I31a563bdea8a5b177f5d86c993577db43745473a
    Signed-off-by: Zhang Dongxing <dongxing.zhang@intel.com>
    Reviewed-on: http://android.intel.com:8080/47575
    Reviewed-by: Lim, Min Yeol <min.yeol.lim@intel.com>
    Reviewed-by: Guerard, Christophe <christophe.guerard@intel.com>
    Reviewed-by: Kanigeri, Hari K <hari.k.kanigeri@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -98,43 +98,43 @@ endif
 	$(RELEASE_PATH)/sync_build/publish_build.py vendor/intel/release/daily_build/sync-build.ini `pwd` $(TARGET_PRODUCT) $(TARGET_BUILD_VARIANT) $(FILE_NAME_TAG)
 
 # sepdk and vTunes
--include $(TOP)/device/intel/PRIVATE/sepdk/src/AndroidSEP.mk
--include $(TOP)/device/intel/PRIVATE/sepdk/src/pax/AndroidPAX.mk
+-include $(TOP)/device/intel/debug_tools/sepdk/src/AndroidSEP.mk
+-include $(TOP)/device/intel/debug_tools/sepdk/src/pax/AndroidPAX.mk
 
 ifeq ($(sepdk),1)
 $(PRODUCT_OUT)/ramdisk.img : sep
 $(PRODUCT_OUT)/ramdisk.img : pax
 # vTune binaries
 PRODUCT_COPY_FILES += \
-        $(TOP)/device/intel/PRIVATE/sepdk/bin/atom_db.txt:/system/bin/sepdk/atom_db.txt \
-        $(TOP)/device/intel/PRIVATE/sepdk/bin/insmod-pax:/system/bin/sepdk/insmod-pax \
-        $(TOP)/device/intel/PRIVATE/sepdk/bin/insmod-sep3:/system/bin/sepdk/insmod-sep3 \
-        $(TOP)/device/intel/PRIVATE/sepdk/bin/rmmod-pax:/system/bin/sepdk/rmmod-pax \
-        $(TOP)/device/intel/PRIVATE/sepdk/bin/rmmod-sep3:/system/bin/sepdk/rmmod-sep3 \
-        $(TOP)/device/intel/PRIVATE/sepdk/bin/sep:/system/bin/sepdk/sep \
-        $(TOP)/device/intel/PRIVATE/sepdk/bin/setup_sep_runtime_env.sh:/system/bin/sepdk/setup_sep_runtime_env.sh \
-        $(TOP)/device/intel/PRIVATE/sepdk/bin/sfdump5:/system/bin/sepdk/sfdump5 \
-        $(TOP)/device/intel/PRIVATE/sepdk/bin/libabstract.so:/system/bin/sepdk/libabstract.so \
-        $(TOP)/device/intel/PRIVATE/sepdk/bin/libatom.so:/system/bin/sepdk/libatom.so \
-        $(TOP)/device/intel/PRIVATE/sepdk/bin/libc_osi.so:/system/bin/sepdk/libc_osi.so \
-        $(TOP)/device/intel/PRIVATE/sepdk/bin/liblogit.so:/system/bin/sepdk/liblogit.so \
-        $(TOP)/device/intel/PRIVATE/sepdk/bin/libpax.so:/system/bin/sepdk/libpax.so \
-        $(TOP)/device/intel/PRIVATE/sepdk/bin/libpise.so:/system/bin/sepdk/libpise.so \
-        $(TOP)/device/intel/PRIVATE/sepdk/bin/libplatform_picker.so:/system/bin/sepdk/libplatform_picker.so \
-        $(TOP)/device/intel/PRIVATE/sepdk/bin/librise.so:/system/bin/sepdk/librise.so \
-        $(TOP)/device/intel/PRIVATE/sepdk/bin/libsampling_utils.so:/system/bin/sepdk/libsampling_utils.so \
-        $(TOP)/device/intel/PRIVATE/sepdk/bin/libsera.so:/system/bin/sepdk/libsera.so \
-        $(TOP)/device/intel/PRIVATE/sepdk/bin/libsmrk.so:/system/bin/sepdk/libsmrk.so \
-        $(TOP)/device/intel/PRIVATE/sepdk/bin/libstrings.so:/system/bin/sepdk/libstrings.so \
-        $(TOP)/device/intel/PRIVATE/sepdk/bin/libtbrw.so:/system/bin/sepdk/libtbrw.so
+        $(TOP)/device/intel/debug_tools/sepdk/bin/atom_db.txt:/system/bin/sepdk/atom_db.txt \
+        $(TOP)/device/intel/debug_tools/sepdk/bin/insmod-pax:/system/bin/sepdk/insmod-pax \
+        $(TOP)/device/intel/debug_tools/sepdk/bin/insmod-sep3:/system/bin/sepdk/insmod-sep3 \
+        $(TOP)/device/intel/debug_tools/sepdk/bin/rmmod-pax:/system/bin/sepdk/rmmod-pax \
+        $(TOP)/device/intel/debug_tools/sepdk/bin/rmmod-sep3:/system/bin/sepdk/rmmod-sep3 \
+        $(TOP)/device/intel/debug_tools/sepdk/bin/sep:/system/bin/sepdk/sep \
+        $(TOP)/device/intel/debug_tools/sepdk/bin/setup_sep_runtime_env.sh:/system/bin/sepdk/setup_sep_runtime_env.sh \
+        $(TOP)/device/intel/debug_tools/sepdk/bin/sfdump5:/system/bin/sepdk/sfdump5 \
+        $(TOP)/device/intel/debug_tools/sepdk/bin/libabstract.so:/system/bin/sepdk/libabstract.so \
+        $(TOP)/device/intel/debug_tools/sepdk/bin/libatom.so:/system/bin/sepdk/libatom.so \
+        $(TOP)/device/intel/debug_tools/sepdk/bin/libc_osi.so:/system/bin/sepdk/libc_osi.so \
+        $(TOP)/device/intel/debug_tools/sepdk/bin/liblogit.so:/system/bin/sepdk/liblogit.so \
+        $(TOP)/device/intel/debug_tools/sepdk/bin/libpax.so:/system/bin/sepdk/libpax.so \
+        $(TOP)/device/intel/debug_tools/sepdk/bin/libpise.so:/system/bin/sepdk/libpise.so \
+        $(TOP)/device/intel/debug_tools/sepdk/bin/libplatform_picker.so:/system/bin/sepdk/libplatform_picker.so \
+        $(TOP)/device/intel/debug_tools/sepdk/bin/librise.so:/system/bin/sepdk/librise.so \
+        $(TOP)/device/intel/debug_tools/sepdk/bin/libsampling_utils.so:/system/bin/sepdk/libsampling_utils.so \
+        $(TOP)/device/intel/debug_tools/sepdk/bin/libsera.so:/system/bin/sepdk/libsera.so \
+        $(TOP)/device/intel/debug_tools/sepdk/bin/libsmrk.so:/system/bin/sepdk/libsmrk.so \
+        $(TOP)/device/intel/debug_tools/sepdk/bin/libstrings.so:/system/bin/sepdk/libstrings.so \
+        $(TOP)/device/intel/debug_tools/sepdk/bin/libtbrw.so:/system/bin/sepdk/libtbrw.so
 endif
 
 # powerdk and wuwatch
--include $(TOP)/device/intel/PRIVATE/debug_tools/powerdk/src/AndroidPowerDK.mk
+-include $(TOP)/device/intel/debug_tools/powerdk/src/AndroidPowerDK.mk
 
 ifeq ($(powerdk),1)
 $(PRODUCT_OUT)/ramdisk.img : apwr
 # Wuwatch binaries
 PRODUCT_COPY_FILES += \
-        $(TOP)/device/intel/PRIVATE/debug_tools/powerdk/bin/wuwatch:/system/bin/powerdk/wuwatch
+        $(TOP)/device/intel/debug_tools/powerdk/bin/wuwatch:/system/bin/powerdk/wuwatch
 endif
