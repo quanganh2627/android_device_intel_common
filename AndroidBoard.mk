8f3c28a4b8ca4c08f2b2b554d1c40cd9b19dd388
jli127 <jian.d.li@intel.com>
2012-05-18 10:37:02 +0800

    Remove gyro, baro and ps sensor feature files
    BZ: 36894
    
    Gyro, baro and ps sensor are not available
    for all platforms. Remove these feature
    files from common board config.
    
    Change-Id: Idba547c871443e60b4b410955230a0dff6ea190a
    Signed-off-by: jli127 <jian.d.li@intel.com>
    Reviewed-on: http://android.intel.com:8080/49294
    Reviewed-by: Wang, Zhifeng <zhifeng.wang@intel.com>
    Reviewed-by: Liu, Hong <hong.liu@intel.com>
    Reviewed-by: Du, Alek <alek.du@intel.com>
    Tested-by: Tang, HaifengX <haifengx.tang@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -73,12 +73,6 @@ PRODUCT_COPY_FILES += \
         $(LOCAL_PATH)/init.sdcard2.rc:root/init.sdcard2.rc \
         $(PERMISSIONS_PATH)/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
         $(PERMISSIONS_PATH)/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
-        $(PERMISSIONS_PATH)/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
-        $(PERMISSIONS_PATH)/android.hardware.sensor.barometer.xml:system/etc/permissions/android.hardware.sensor.barometer.xml \
-        $(PERMISSIONS_PATH)/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
-        $(PERMISSIONS_PATH)/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
-        $(PERMISSIONS_PATH)/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
-        $(PERMISSIONS_PATH)/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
         $(PERMISSIONS_PATH)/android.hardware.touchscreen.xml:system/etc/permissions/android.hardware.touchscreen.xml \
         $(PERMISSIONS_PATH)/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml \
         $(PERMISSIONS_PATH)/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
