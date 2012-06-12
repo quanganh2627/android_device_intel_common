41e2f67907198aeda97b769f16d5f79fb614fa9a
Jeremie GARCIA <jeremiex.garcia@intel.com>
2012-06-12 18:32:52 +0200

    wlan: wifi configuration factorization improvements and cleanup
    BZ: 41536
    
    This patch improves wifi configuration factorization:
      - add header to each ".conf" and ".rc" file to have user friendly final files.
        Reading final files, it becomes easy to determine which part is common to every platform/SOC or variant specific
      - platform configuration are now hosted in WifiBoardConfig.mk that are included in BoardConfg.mk. We can now build supplicant under source code with "mm" command.
      - SOC level defines which wifi chip we use
      - variant level enables/disables Wifi/Widi feature
      - cleanup of variables and reorganizing to clear ".mk" files
      - cleanup of dependencies:
          * bootimage: build and export wifi "init.xxx.rc" files
          * systemtarball: build and export wifi ".conf" files
    
    Change-Id: Icd72833aa07e6c9efc9d7491ef80c88b112d7720
    Signed-off-by: Jeremie GARCIA <jeremiex.garcia@intel.com>
    Reviewed-on: http://android.intel.com:8080/52537
    Reviewed-by: Trivelly, Jean <jean.trivelly@intel.com>
    Reviewed-by: Champciaux, Nicolas <nicolas.champciaux@intel.com>
    Reviewed-by: Tricot, HonoreX <honorex.tricot@intel.com>
    Tested-by: Champciaux, Nicolas <nicolas.champciaux@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

---
+++ wifi/WifiBoardConfig.mk
@@ -0,0 +1,27 @@
+
+# TI12xx serie wifi specific
+ifeq ($(strip $(BOARD_HAVE_TI12XX)),true)
+BOARD_WLAN_DEVICE              := wl12xx-compat
+BOARD_WPA_SUPPLICANT_DRIVER    := NL80211
+WPA_SUPPLICANT_VERSION         := VER_0_8_X
+ADDITIONAL_DEFAULT_PROPERTIES  += wifi.interface=wlan0:0
+endif
+
+# BCM43xx serie wifi specific
+ifeq ($(strip $(BOARD_HAVE_BCM43XX)),true)
+BOARD_WLAN_DEVICE              := bcmdhd
+BOARD_WPA_SUPPLICANT_DRIVER    := NL80211
+WPA_SUPPLICANT_VERSION         := VER_0_8_X
+ADDITIONAL_DEFAULT_PROPERTIES  += wifi.interface=wlan0
+
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_HOSTAPD_DRIVER        := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB   := lib_driver_cmd_bcmdhd
+WIFI_DRIVER_MODULE_ARG      := "firmware_path=/system/etc/firmware/fw_bcmdhd.bin nvram_path=/system/etc/wifi/bcmdhd.cal"
+WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/bcmdhd/parameters/firmware_path"
+WIFI_DRIVER_MODULE_PATH     := "/lib/modules/bcmdhd.ko"
+WIFI_DRIVER_FW_PATH_STA     := "/vendor/firmware/fw_bcmdhd.bin"
+WIFI_DRIVER_FW_PATH_P2P     := "/vendor/firmware/fw_bcmdhd.bin"
+WIFI_DRIVER_FW_PATH_AP      := "/vendor/firmware/fw_bcmdhd.bin"
+WIFI_DRIVER_MODULE_NAME     := bcmdhd
+endif
--- wifi/WifiRules.mk
+++ wifi/WifiRules.mk
@@ -1,8 +1,14 @@
+####################################
+# Paths declaration
+####################################
 
 COMMON_WIFI_DIR = vendor/intel/common/wifi
 LOCAL_COMMON_WIFI_DIR = $(PWD)/$(COMMON_WIFI_DIR)
 
-# wifi configuration file names
+####################################
+# Configuration filenames
+####################################
+
 STA_CONF_FILE_NAME      = wpa_supplicant.conf
 P2P_CONF_FILE_NAME      = p2p_supplicant.conf
 HOSTAPD_CONF_FILE_NAME  = hostapd.conf
@@ -12,7 +18,10 @@ WIDI_CONF_FILE_NAME     = widi_supplicant.conf
 WIDI_INIT_RC_FILE_NAME  = init.widi.rc
 endif
 
-# target paths and names
+####################################
+# Files path on target
+####################################
+
 CONF_PATH_ON_TARGET = system/etc/wifi
 STA_CONF_PATH_ON_TARGET       = $(CONF_PATH_ON_TARGET)/$(STA_CONF_FILE_NAME)
 P2P_CONF_PATH_ON_TARGET       = $(CONF_PATH_ON_TARGET)/$(P2P_CONF_FILE_NAME)
@@ -21,7 +30,11 @@ ifeq ($(strip $(INTEL_WIDI)),true)
 WIDI_CONF_PATH_ON_TARGET = $(CONF_PATH_ON_TARGET)/$(WIDI_CONF_FILE_NAME)
 endif
 
-# wifi common configuration files
+####################################
+# Configuration files
+####################################
+
+# common configuration files
 STA_CONF_FILES      += $(LOCAL_COMMON_WIFI_DIR)/$(STA_CONF_FILE_NAME)
 P2P_CONF_FILES      += $(LOCAL_COMMON_WIFI_DIR)/$(P2P_CONF_FILE_NAME)
 HOSTAPD_CONF_FILES  += $(LOCAL_COMMON_WIFI_DIR)/$(HOSTAPD_CONF_FILE_NAME)
@@ -29,12 +42,54 @@ ifeq ($(strip $(INTEL_WIDI)),true)
 WIDI_CONF_FILES     += $(LOCAL_COMMON_WIFI_DIR)/$(WIDI_CONF_FILE_NAME)
 endif
 
-# targets names
-WIFI_CONF_TARGET = wifi_conf
-ifeq ($(strip $(INTEL_WIDI)),true)
-WIDI_CONF_TARGET = widi_conf
+####################################
+# Product files to copy
+####################################
+
+# common product files to copy
+PRODUCT_COPY_FILES += \
+   $(PERMISSIONS_PATH)/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+   $(PERMISSIONS_PATH)/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml
+
+# manufacturer specific files to copy
+ifeq ($(BOARD_WLAN_DEVICE),wl12xx-compat)
+PRODUCT_COPY_FILES += \
+        $(wildcard $(VARIANT_PATH)/wifi/TQS*ini):system/etc/wifi/TQS.ini
 endif
 
+####################################
+# init files
+####################################
+
+PRODUCT_WIFI_RC_FILES += \
+	$(COMMON_WIFI_DIR)/$(WIFI_INIT_RC_FILE_NAME)
+
+ifeq ($(strip $(INTEL_WIDI)),true)
+PRODUCT_WIDI_RC_FILES += \
+	 $(COMMON_WIFI_DIR)/$(WIDI_INIT_RC_FILE_NAME)
+endif
+
+####################################
+# Includes
+####################################
+
+ifeq ($(BOARD_WLAN_DEVICE),wl12xx-compat)
+-include $(TOP)/hardware/ti/wlan/wl12xx-compat/AndroidWl12xxCompat.mk
+endif
+
+####################################
+# Rules and targets
+####################################
+
+# targets names
+WIFI_CONF_TARGET = wifi_conf
+WIFI_RC_TARGET   = wifi_rc
+ifeq ($(strip $(INTEL_WIDI)),true)
+WIDI_CONF_TARGET = widi_conf
+WIDI_RC_TARGET   = widi_rc
+endif
+
+# common targets and rules
 $(PRODUCT_OUT)/$(STA_CONF_PATH_ON_TARGET):
 	mkdir -p $(PRODUCT_OUT)/$(CONF_PATH_ON_TARGET)
 	cat $(STA_CONF_FILES) > $(PRODUCT_OUT)/$(STA_CONF_PATH_ON_TARGET)
@@ -47,46 +102,32 @@ $(PRODUCT_OUT)/$(HOSTAPD_CONF_PATH_ON_TARGET):
 	mkdir -p $(PRODUCT_OUT)/$(CONF_PATH_ON_TARGET)
 	cat $(HOSTAPD_CONF_FILES) > $(PRODUCT_OUT)/$(HOSTAPD_CONF_PATH_ON_TARGET)
 
-ifeq ($(strip $(INTEL_WIDI)),true)
 $(PRODUCT_OUT)/$(WIDI_CONF_PATH_ON_TARGET):
 	mkdir -p $(PRODUCT_OUT)/$(CONF_PATH_ON_TARGET)
 	cat $(WIDI_CONF_FILES) > $(PRODUCT_OUT)/$(WIDI_CONF_PATH_ON_TARGET)
-endif
 
-wifi_rc:
+$(WIFI_RC_TARGET):
 	mkdir -p $(PRODUCT_OUT)/root
 	cat $(PRODUCT_WIFI_RC_FILES) > $(PRODUCT_OUT)/root/init.wifi.rc
 
-widi_rc:
+$(WIDI_RC_TARGET):
 	mkdir -p $(PRODUCT_OUT)/root
 	cat $(PRODUCT_WIDI_RC_FILES) > $(PRODUCT_OUT)/root/init.widi.rc
 
-wifi_conf: $(PRODUCT_OUT)/$(STA_CONF_PATH_ON_TARGET)  \
+$(WIFI_CONF_TARGET): $(PRODUCT_OUT)/$(STA_CONF_PATH_ON_TARGET)  \
            $(PRODUCT_OUT)/$(P2P_CONF_PATH_ON_TARGET)  \
-           $(PRODUCT_OUT)/$(HOSTAPD_CONF_PATH_ON_TARGET) \
-           wifi_rc
+           $(PRODUCT_OUT)/$(HOSTAPD_CONF_PATH_ON_TARGET)
 
+$(WIDI_CONF_TARGET): $(PRODUCT_OUT)/$(WIDI_CONF_PATH_ON_TARGET)
+
+bootimage:    $(WIFI_RC_TARGET)
+systemimg_gz: $(WIFI_CONF_TARGET)
 ifeq ($(strip $(INTEL_WIDI)),true)
-widi_conf: $(PRODUCT_OUT)/$(WIDI_CONF_PATH_ON_TARGET) \
-           widi_rc
+bootimage:    $(WIDI_RC_TARGET)
+systemimg_gz: $(WIDI_CONF_TARGET)
 endif
 
-# default wifi
-BOARD_WLAN_DEVICE              := wl12xx-compat
-BOARD_WPA_SUPPLICANT_DRIVER    := NL80211
-WPA_SUPPLICANT_VERSION         := VER_0_8_X
-ADDITIONAL_DEFAULT_PROPERTIES  += wifi.interface=wlan0:0
-
--include $(TOP)/hardware/ti/wlan/wl12xx-compat/AndroidWl12xxCompat.mk
+# manufacturer specific targets and rules
+ifeq ($(BOARD_WLAN_DEVICE),wl12xx-compat)
 $(PRODUCT_OUT)/ramdisk.img : build_wl12xx-compat
-
-# product copy files
-PRODUCT_COPY_FILES += \
-   $(PERMISSIONS_PATH)/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
-   $(PERMISSIONS_PATH)/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml
-
-# init.xxx.rc
-PRODUCT_WIFI_RC_FILES += $(COMMON_WIFI_DIR)/$(WIFI_INIT_RC_FILE_NAME)
-ifeq ($(strip $(INTEL_WIDI)),true)
-PRODUCT_WIDI_RC_FILES += $(COMMON_WIFI_DIR)/$(WIDI_INIT_RC_FILE_NAME)
 endif
