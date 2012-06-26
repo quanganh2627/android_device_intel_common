52642935cc939a62d6f39f62cbf94978c4f5e448
Jeremie GARCIA <jeremiex.garcia@intel.com>
2012-06-26 16:58:10 +0200

    [PORT FROM R3]wlan: set default regulatory domain (new flavor)
    BZ: 43713 41553
    
    This patch intends to provide a new way of generating and storing the default regulatory domain.
    
    It has first been done using the ieee80211_regdom parameter of module cfg80211.
    However this introduced unmanaged side effects (reboot with wifi on make regulatory domain set every 3s with country 97).
    Investigating this issue across linux-wireless, we discovered that this parameter is deprecated and should no more be used.
    
    http://linuxwireless.org/en/developers/Regulatory/#The_ieee80211_regdom_module_parameter
    
    To resolve the issue and comply with linux-wireless recommandations, we use the "country" parameter of wpa_supplicant configuration file.
    With this patch all wpa_supplicant conf files (wpa/p2p/widi_supplicant.conf) have their country parameter set at build-time with the content of "PRODUCT_LOCALES" build variable. This variable conditions the value of "ro.product.locale.region" android property.
    
    So the logic is:
    1- default value = $PRODUCT_LOCALES
    2- if telephony feature enabled, override regdom with the one extracted from SIM (if any)
    3- if Wifi AP sends a country code => override with the country code received
    
    Change-Id: I8ad2c072182ebbbfbdbc8bb53ee7feed8b31ad08
    Orig-Change-Id: I94e237dd6f96d3b3b2321c04dfbf482d689c496c
    Signed-off-by: Jeremie GARCIA <jeremiex.garcia@intel.com>
    Reviewed-on: http://android.intel.com:8080/54375
    Reviewed-by: Trivelly, Jean <jean.trivelly@intel.com>
    Reviewed-by: Tardy, Pierre <pierre.tardy@intel.com>
    Reviewed-by: Marotte, Fabien <fabien.marotte@intel.com>
    Reviewed-by: Bachot, Jean-michel <jean-michel.bachot@intel.com>
    Tested-by: Champciaux, Nicolas <nicolas.champciaux@intel.com>
    Reviewed-by: Seibel, Eric <eric.seibel@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- wifi/WifiRules.mk
+++ wifi/WifiRules.mk
@@ -43,6 +43,17 @@ WIDI_CONF_FILES     += $(LOCAL_COMMON_WIFI_DIR)/$(WIDI_CONF_FILE_NAME)
 endif
 
 ####################################
+# Locale variables
+####################################
+
+REGDOM=$(word 2,$(subst _, ,$(word 1,$(PRODUCT_LOCALES))))
+MV=mv
+CAT=cat
+SED=sed
+MKDIR=mkdir
+GREP=grep
+
+####################################
 # Product files to copy
 ####################################
 
@@ -77,6 +88,21 @@ ifeq ($(BOARD_WLAN_DEVICE),wl12xx-compat)
 -include $(TOP)/hardware/ti/wlan/wl12xx-compat/AndroidWl12xxCompat.mk
 endif
 
+
+####################################
+# Functions
+####################################
+
+define set_def_regdom
+	$(SED) 's/country=XY/country=$(REGDOM)/g' $1 > $1.tmp
+	$(MV) $1.tmp $1
+endef
+
+define clean_conf_file
+	$(CAT) $1 | $(GREP) -v "(todel)" > $1.tmp
+	$(MV) $1.tmp $1
+endef
+
 ####################################
 # Rules and targets
 ####################################
@@ -91,28 +117,34 @@ endif
 
 # common targets and rules
 $(PRODUCT_OUT)/$(STA_CONF_PATH_ON_TARGET):
-	mkdir -p $(PRODUCT_OUT)/$(CONF_PATH_ON_TARGET)
-	cat $(STA_CONF_FILES) > $(PRODUCT_OUT)/$(STA_CONF_PATH_ON_TARGET)
+	$(MKDIR) -p $(PRODUCT_OUT)/$(CONF_PATH_ON_TARGET)
+	$(CAT) $(STA_CONF_FILES) > $(PRODUCT_OUT)/$(STA_CONF_PATH_ON_TARGET)
+	$(call set_def_regdom,$(PRODUCT_OUT)/$(STA_CONF_PATH_ON_TARGET))
+	$(call clean_conf_file,$(PRODUCT_OUT)/$(STA_CONF_PATH_ON_TARGET))
 
 $(PRODUCT_OUT)/$(P2P_CONF_PATH_ON_TARGET):
-	mkdir -p $(PRODUCT_OUT)/$(CONF_PATH_ON_TARGET)
-	cat $(P2P_CONF_FILES) > $(PRODUCT_OUT)/$(P2P_CONF_PATH_ON_TARGET)
+	$(MKDIR) -p $(PRODUCT_OUT)/$(CONF_PATH_ON_TARGET)
+	$(CAT) $(P2P_CONF_FILES) > $(PRODUCT_OUT)/$(P2P_CONF_PATH_ON_TARGET)
+	$(call set_def_regdom,$(PRODUCT_OUT)/$(P2P_CONF_PATH_ON_TARGET))
+	$(call clean_conf_file,$(PRODUCT_OUT)/$(P2P_CONF_PATH_ON_TARGET))
 
 $(PRODUCT_OUT)/$(HOSTAPD_CONF_PATH_ON_TARGET):
-	mkdir -p $(PRODUCT_OUT)/$(CONF_PATH_ON_TARGET)
-	cat $(HOSTAPD_CONF_FILES) > $(PRODUCT_OUT)/$(HOSTAPD_CONF_PATH_ON_TARGET)
+	$(MKDIR) -p $(PRODUCT_OUT)/$(CONF_PATH_ON_TARGET)
+	$(CAT) $(HOSTAPD_CONF_FILES) > $(PRODUCT_OUT)/$(HOSTAPD_CONF_PATH_ON_TARGET)
 
 $(PRODUCT_OUT)/$(WIDI_CONF_PATH_ON_TARGET):
-	mkdir -p $(PRODUCT_OUT)/$(CONF_PATH_ON_TARGET)
-	cat $(WIDI_CONF_FILES) > $(PRODUCT_OUT)/$(WIDI_CONF_PATH_ON_TARGET)
+	$(MKDIR) -p $(PRODUCT_OUT)/$(CONF_PATH_ON_TARGET)
+	$(CAT) $(WIDI_CONF_FILES) > $(PRODUCT_OUT)/$(WIDI_CONF_PATH_ON_TARGET)
+	$(call set_def_regdom,$(PRODUCT_OUT)/$(WIDI_CONF_PATH_ON_TARGET))
+	$(call clean_conf_file,$(PRODUCT_OUT)/$(WIDI_CONF_PATH_ON_TARGET))
 
 $(WIFI_RC_TARGET):
-	mkdir -p $(PRODUCT_OUT)/root
-	cat $(PRODUCT_WIFI_RC_FILES) > $(PRODUCT_OUT)/root/init.wifi.rc
+	$(MKDIR) -p $(PRODUCT_OUT)/root
+	$(CAT) $(PRODUCT_WIFI_RC_FILES) > $(PRODUCT_OUT)/root/init.wifi.rc
 
 $(WIDI_RC_TARGET):
-	mkdir -p $(PRODUCT_OUT)/root
-	cat $(PRODUCT_WIDI_RC_FILES) > $(PRODUCT_OUT)/root/init.widi.rc
+	$(MKDIR) -p $(PRODUCT_OUT)/root
+	$(CAT) $(PRODUCT_WIDI_RC_FILES) > $(PRODUCT_OUT)/root/init.widi.rc
 
 $(WIFI_CONF_TARGET): $(PRODUCT_OUT)/$(STA_CONF_PATH_ON_TARGET)  \
            $(PRODUCT_OUT)/$(P2P_CONF_PATH_ON_TARGET)  \
