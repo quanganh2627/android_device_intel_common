54832f54c8b2bea33076e4bde9be2fdf9e27b85b
Bruce Beare <bruce.j.beare@intel.com>
2012-07-14 00:03:13 -0700

    PRODUCT_COPY_FILES: Disable incorrect use
    BZ: 46719
    
    This makfile variable is only able to be set in "product.mk" files -- never
    in Android.mk or AndroidBoard.mk files.
    
    Change-Id: I28dd32bc0f39661079ce09563f758037c2945565
    Reviewed-on: http://android.intel.com:8080/56437
    Reviewed-by: Beare, Bruce J <bruce.j.beare@intel.com>
    Reviewed-by: Li, XiaojingX <xiaojingx.li@intel.com>
    Tested-by: Li, XiaojingX <xiaojingx.li@intel.com>

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -52,30 +52,6 @@ PRODUCT_INIT_RC_FILES += $(COMMON_PATH)/init.common.rc
 # OOM Killer tweaks
 #PRODUCT_INIT_RC_FILES += vendor/intel/common/init.mfld.oom.rc
 
-# board specific files
-PRODUCT_COPY_FILES += \
-        $(LOCAL_PATH)/android.conf:system/etc/dhcpcd/android.conf \
-        $(LOCAL_PATH)/activate_trace_modem:system/bin/activate_trace_modem \
-        $(LOCAL_PATH)/configure_trace_modem:system/bin/configure_trace_modem \
-        $(LOCAL_PATH)/usbswitch_modem:system/bin/usbswitch_modem \
-        $(LOCAL_PATH)/usbswitch_ape:system/bin/usbswitch_ape \
-        $(LOCAL_PATH)/usbswitch_status:system/bin/usbswitch_status \
-        $(LOCAL_PATH)/toggle_pin_on1:system/bin/toggle_pin_on1 \
-        $(LOCAL_PATH)/ueventd.common.rc:root/ueventd.$(TARGET_PRODUCT).rc
-
-# platform common files
-PRODUCT_COPY_FILES += \
-        $(LOCAL_PATH)/init.rc:init.rc \
-        $(PERMISSIONS_PATH)/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
-        $(PERMISSIONS_PATH)/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
-        $(PERMISSIONS_PATH)/android.hardware.touchscreen.xml:system/etc/permissions/android.hardware.touchscreen.xml \
-        $(PERMISSIONS_PATH)/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml \
-        $(PERMISSIONS_PATH)/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
-        $(PERMISSIONS_PATH)/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
-        $(PERMISSIONS_PATH)/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
-        $(PERMISSIONS_PATH)/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml
-
-
 flashfiles: bootimage
 ifneq ($(FLASHFILE_BOOTONLY),true)
 ifeq ($(FLASHFILE_NO_OTA),true)
