04110d7cb95ca6cb58f3ff2a075d6c12487b99dc
Romieu, Benoit <benoit.romieu@intel.com>
2012-07-17 22:23:55 -0700

    Revert "[REVERTME] hack for jb boot camp: use droidboot ics prebuilt binary"
    BZ: 46856
    
    This reverts commit fc5bb1e8f9291a143a167b5441c981b6bd11f608
    
    Change-Id: Ie5c2977c04c166991e355e930bf012e5586585ab
    Reviewed-on: http://android.intel.com:8080/56857
    Reviewed-by: Imberton, Guilhem <guilhem.imberton@intel.com>
    Reviewed-by: Pince, Jean-christophe <jean-christophe.pince@intel.com>
    Tested-by: Touzeau, GabrielX <gabrielx.touzeau@intel.com>
    Reviewed-by: Monnier, Olivier <olivier.monnier@intel.com>
    Reviewed-by: Li, XiaojingX <xiaojingx.li@intel.com>
    Tested-by: Li, XiaojingX <xiaojingx.li@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -80,8 +80,7 @@ blank_flashfiles: droidbootimage
 else
 blank_flashfiles: recoveryimage
 endif
-blank_flashfiles: $(PREBUILT_DROIDBOOT_IMG) | $(ACP)
-	$(ACP) $(PREBUILT_DROIDBOOT_IMG) $(PRODUCT_OUT)
+blank_flashfiles:
 	$(if $(IFWI_PREBUILT_PATHS), \
 		PUBLISH_PATH=$(PUBLISH_PATH) \
 		TARGET_PUBLISH_PATH=$(PUBLISH_PATH)/$(TARGET_PUBLISH_PATH) \
