ce1013d5641c6d5baaea01b4e162f8861dcbbc6b
Olivier Monnier <olivier.monnier@intel.com>
2012-09-05 09:28:21 +0200

    Add checkapi target as droid dependency
    BZ: 59356
    
    checkapi is only called if droid is among the cmd goals, or no cmd goal is given
    We want it to be called for other targets as well
    
    Change-Id: I250957ddc62e800f2e39d3c3a84e3b21e0ac5291
    Signed-off-by: Olivier Monnier <olivier.monnier@intel.com>
    Reviewed-on: http://android.intel.com:8080/67999
    Reviewed-by: Alberdi, IonX <ionx.alberdi@intel.com>
    Reviewed-by: Protat, RemyX <remyx.protat@intel.com>
    Reviewed-by: Mahe, Erwan <erwan.mahe@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -46,6 +46,10 @@ MKBOOTIMG := vendor/intel/support/mkbootimg
 $(INSTALLED_KERNEL_TARGET): build_kernel
 $(INSTALLED_RAMDISK_TARGET): build_kernel
 
+# checkapi is only called if droid is among the cmd goals, or no cmd goal is given
+# We add it here to be called for other targets as well
+droid: checkapi
+
 flashfiles: bootimage
 ifneq ($(FLASHFILE_BOOTONLY),true)
 ifeq ($(FLASHFILE_NO_OTA),true)
