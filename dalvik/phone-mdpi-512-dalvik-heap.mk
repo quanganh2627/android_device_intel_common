32181033d065ae2db3d58796dbb93b8ab8cc221d
Qiming Shi <qiming.shi@intel.com>
2012-12-10 20:09:22 +0800

    Add 3 more dalvik heap configuration to .mk files
    BZ: 73800
    
    Change-Id: Ie013d184c6af2fc1c76e6e4a90a66d6c9a0bcf40
    Signed-off-by: Qiming Shi <qiming.shi@intel.com>
    Reviewed-on: http://android.intel.com:8080/80263
    Reviewed-by: Imberton, Guilhem <guilhem.imberton@intel.com>
    Tested-by: Li, XiaojingX <xiaojingx.li@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- dalvik/phone-mdpi-512-dalvik-heap.mk
+++ dalvik/phone-mdpi-512-dalvik-heap.mk
@@ -1,4 +1,7 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapstartsize=5m \
     dalvik.vm.heapgrowthlimit=48m \
-    dalvik.vm.heapsize=128m
+    dalvik.vm.heapsize=128m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=2m \
+    dalvik.vm.heapmaxfree=2m
