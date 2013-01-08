e0356e9f54c51192e3fb957f0b57b7f5cb595a49
Nicolas SUET <nicolas.suet@intel.com>
2013-01-08 18:08:01 +0100

    GPS: Integration of Broadcom GPS: BCM4752 - common
    BZ: 84010
    
    Enable BCM4752 GPS.
    
    Change-Id: I2828c6717a6bf8e4930efcafe25260398cd8b039
    Signed-off-by: Nicolas SUET <nicolas.suet@intel.com>
    Reviewed-on: http://android.intel.com:8080/89415
    Reviewed-by: Chiotti, MarcoX <marcox.chiotti@intel.com>
    Reviewed-by: Peix, Fabien <fabien.peix@intel.com>
    Reviewed-by: Pantaloni, JeromeX <jeromex.pantaloni@intel.com>
    Reviewed-by: Seibel, Eric <eric.seibel@intel.com>
    Tested-by: Champciaux, Nicolas <nicolas.champciaux@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- gps/bcm/Android.mk
+++ gps/bcm/Android.mk
@@ -3,13 +3,36 @@ VENDOR_PATH := bcm
 ##################################################
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := init.gps.rc
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
+LOCAL_SRC_FILES := $(VENDOR_PATH)/init.gps.rc
+include $(BUILD_PREBUILT)
+
+##################################################
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := init.gps.io.rc
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
+LOCAL_SRC_FILES := $(PLATFORM_PATH)/gps/$(GPS_CHIP_VENDOR)/init.gps.io.rc
+include $(BUILD_PREBUILT)
+
+##################################################
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := gps_bcm_4752
 LOCAL_MODULE_TAGS := optional
-#LOCAL_REQUIRED_MODULES :=  \
+LOCAL_REQUIRED_MODULES :=  \
+    gps_common \
+    init.gps.io.rc \
+    gpsd \
+    gpslogd \
+    gps.conf \
+    gps.xml
 
-# WARNING: To be kept as the last required module.
-LOCAL_REQUIRED_MODULES +=  \
-    gps_common
 include $(BUILD_PHONY_PACKAGE)
 
 ##################################################
