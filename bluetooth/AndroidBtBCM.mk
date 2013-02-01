4aa4780b6b7075b1abcf7e568dab645626967a3c
Xavier Boniface <xavierx.boniface@intel.com>
2013-02-01 11:40:47 +0100

    [BT] Copy all Bluetooth Broadcom chips' firmware files to target
    BZ: 83970
    
    This allows a user to swap AOBs on VV boards and this will work with the same sw.
    Notes:
      - chip autodetection is handled in device/common/libbt/src/hardware.c (see
        hw_config_cback(); READ_LOCAL_NAME)
      - chip autodetection works only for Broadcom chips.
    
    Change-Id: I9c928165f5ff32f39997ec3737549888251f5999
    Signed-off-by: Xavier Boniface <xavierx.boniface@intel.com>
    Reviewed-on: http://android.intel.com:8080/89819
    Reviewed-by: Seibel, Eric <eric.seibel@intel.com>
    Reviewed-by: Bondier, CedricX <cedricx.bondier@intel.com>
    Tested-by: Champciaux, Nicolas <nicolas.champciaux@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- bluetooth/AndroidBtBCM.mk
+++ bluetooth/AndroidBtBCM.mk
@@ -11,6 +11,8 @@ LOCAL_MODULE := bt_bcm
 LOCAL_MODULE_TAGS := optional
 LOCAL_REQUIRED_MODULES :=  \
     BCM4334B0_002.001.013.0636.0641_RC.hcd \
+    BCM4335A0_001.001.038.0035.0000_TestCfg_Intel_Merrifield.hcd \
+    BCM4335B0_002.001.006.0031.0034_RC.hcd \
     bt_common
 
 include $(BUILD_PHONY_PACKAGE)
@@ -23,6 +25,26 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(BT_FW_TARGET_PATH)
 LOCAL_SRC_FILES := $(BCM4334_BT_FW_PATH)/BCM4334B0_002.001.013.0636.0641_RC.hcd
+include $(BUILD_PREBUILT)
+
+##################################################
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := BCM4335A0_001.001.038.0035.0000_TestCfg_Intel_Merrifield.hcd
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(BT_FW_TARGET_PATH)
+LOCAL_SRC_FILES := $(BCM4334_BT_FW_PATH)/BCM4335A0_001.001.038.0035.0000_TestCfg_Intel_Merrifield.hcd
+include $(BUILD_PREBUILT)
+
+##################################################
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := BCM4335B0_002.001.006.0031.0034_RC.hcd
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(BT_FW_TARGET_PATH)
+LOCAL_SRC_FILES := $(BCM4334_BT_FW_PATH)/BCM4335B0_002.001.006.0031.0034_RC.hcd
 include $(BUILD_PREBUILT)
 
 ##################################################
