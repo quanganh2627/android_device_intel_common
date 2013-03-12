d73e57356fd31ba543e0387d0c911c26e72bcdbe
Cedric Bondier <cedricx.bondier@intel.com>
2013-03-12 18:13:04 +0100

    [BT] 43241 - B0 H/W module BT FW Integration
    BZ: 92043
    
    Integration into mainline of BCM43241 firmware for the targeted
    B0 hardware module.
    
    Change-Id: Iddbba5654634becb65f96112e3ce34eba299e12a
    Signed-off-by: Cedric Bondier <cedricx.bondier@intel.com>
    Reviewed-on: http://android.intel.com:8080/96232
    Reviewed-by: Gros, Julien <julien.gros@intel.com>
    Reviewed-by: Aquilone, RaffaeleX <raffaelex.aquilone@intel.com>
    Reviewed-by: Imberton, Guilhem <guilhem.imberton@intel.com>
    Tested-by: Zurmely, PierreX <pierrex.zurmely@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- bluetooth/AndroidBtBCM.mk
+++ bluetooth/AndroidBtBCM.mk
@@ -9,6 +9,7 @@ LOCAL_REQUIRED_MODULES :=  \
     BCM4334B0_002.001.013.0636.0641_RC.hcd \
     BCM4335A0_001.001.038.0035.0000_TestCfg_Intel_Merrifield.hcd \
     BCM4335B0_002.001.006.0031.0034_RC.hcd \
+    BCM43241B0_0082_Intel_BayTrail_TEST.hcd \
     bt_common
 
 include $(BUILD_PHONY_PACKAGE)
