2da64d075f9ef74b6a0fdcfcfa5c13d1aaefdc2f
Jean Trivelly <jean.trivelly@intel.com>
2013-06-20 18:45:11 +0200

    WiFi: Modify the makefiles for License purpose
    BZ: 110982
    
    Packages declaration used for Firmware and NVRAM
    copy are now located into Firmware repositories.
    In addition, factorize all wifi chip packages declaration
    into the same makefile.
    
    Change-Id: I984fd7727b98baa67da153ba2722ea360f9ae8fd
    Signed-off-by: Jean Trivelly <jean.trivelly@intel.com>
    Reviewed-on: http://android.intel.com:8080/114702
    Reviewed-by: Bachot, Jean-michel <jean-michel.bachot@intel.com>
    Reviewed-by: Garcia, JeremieX <jeremiex.garcia@intel.com>
    Reviewed-by: Imberton, Guilhem <guilhem.imberton@intel.com>
    Tested-by: Lestienne, ChristopheX <christophex.lestienne@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- wifi/AndroidWifiBCM.mk
+++ wifi/AndroidWifiBCM.mk
@@ -21,6 +21,75 @@ LOCAL_REQUIRED_MODULES +=   \
 # WARNING: To be kept as the last required module.
 LOCAL_REQUIRED_MODULES +=  \
     wifi_common
-
 include $(BUILD_PHONY_PACKAGE)
 
+##################################################
+################## WiFi BCM 4334 #################
+include $(CLEAR_VARS)
+LOCAL_MODULE := wifi_bcm_4334
+LOCAL_MODULE_TAGS := optional
+LOCAL_REQUIRED_MODULES :=               \
+    fw_bcmdhd.bin_4334_b2               \
+    bcmdhd_aob.cal_4334_b2              \
+    bcmdhd_victoriabay_prx.cal_4334_b2  \
+    bcmdhd_bodegabay_pr1.cal_4334_b2
+
+LOCAL_REQUIRED_MODULES +=  \
+    wifi_bcm
+include $(BUILD_PHONY_PACKAGE)
+
+##################################################
+############ WiFi BCM 43340 or 43341 #############
+include $(CLEAR_VARS)
+LOCAL_MODULE := wifi_bcm_4334x
+LOCAL_MODULE_TAGS := optional
+LOCAL_REQUIRED_MODULES :=  \
+    fw_bcmdhd.bin_4334x_b0 \
+    bcmdhd_aob.cal_4334x_b0
+
+LOCAL_REQUIRED_MODULES +=  \
+    wifi_bcm
+include $(BUILD_PHONY_PACKAGE)
+
+##################################################
+################# WiFi BCM 43241 #################
+include $(CLEAR_VARS)
+LOCAL_MODULE := wifi_bcm_43241
+LOCAL_MODULE_TAGS := optional
+LOCAL_REQUIRED_MODULES :=   \
+    fw_bcmdhd.bin_43241_b0  \
+    fw_bcmdhd.bin_43241_b4  \
+    bcmdhd_aob.cal_43241_b0 \
+    bcmdhd_aob.cal_43241_b4
+
+LOCAL_REQUIRED_MODULES +=   \
+    wifi_bcm
+include $(BUILD_PHONY_PACKAGE)
+
+##################################################
+################## WiFi BCM 4335 #################
+include $(CLEAR_VARS)
+LOCAL_MODULE := wifi_bcm_4335
+LOCAL_MODULE_TAGS := optional
+LOCAL_REQUIRED_MODULES :=  \
+    fw_bcmdhd.bin_4335_b0  \
+    bcmdhd_aob.cal_4335_b0
+
+LOCAL_REQUIRED_MODULES +=  \
+    wifi_bcm
+include $(BUILD_PHONY_PACKAGE)
+
+##################################################
+################## WiFi BCM 4339 #################
+include $(CLEAR_VARS)
+LOCAL_MODULE := wifi_bcm_4339
+LOCAL_MODULE_TAGS := optional
+LOCAL_REQUIRED_MODULES :=  \
+    fw_bcmdhd.bin_4339_a0  \
+    bcmdhd_aob.cal_4339_a0
+
+LOCAL_REQUIRED_MODULES +=  \
+    wifi_bcm
+include $(BUILD_PHONY_PACKAGE)
+
+##################################################
