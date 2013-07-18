c21e13f5b4bef2ebdbc9a5448c6ecf490adf562b
Qiming Shi <qiming.shi@intel.com>
2013-07-18 16:33:52 +0800

    Dalvik: Add Dalvik Configuration for 10-inch platform
    BZ: 124154
    
    Currently on BYT ffrd10 platform, we still use previous dalvik heap configuration
    for CTP, which is not suitable for larger screen scenaio and not compatible to
    Google's recommendation.
    
    Add this new configuration for 10 inch platfrom likt BYT ffrd10.
    
    Change-Id: I2327ed132ff71eb0566e1c601ee113b5d8d5c207
    Signed-off-by: Qiming Shi <qiming.shi@intel.com>
    Reviewed-on: http://android.intel.com:8080/119416
    Reviewed-by: Chang, Yang <yang.chang@intel.com>
    Reviewed-by: Imberton, Guilhem <guilhem.imberton@intel.com>
    Tested-by: Chen, HuaqiangX <huaqiangx.chen@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

---
+++ dalvik/tablet-10in-xhdpi-2048-dalvik-heap.mk
@@ -0,0 +1,8 @@
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapstartsize=16m \
+    dalvik.vm.heapgrowthlimit=192m \
+    dalvik.vm.heapsize=512m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=2m \
+    dalvik.vm.heapmaxfree=8m \
+    dalvik.jit.code_cache_size=1048576
