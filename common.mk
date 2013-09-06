37797a2c3f10d4b4b3d42301d693beaeba831954
buildslave <buildslave@buildbot.tl.intel.com>
2013-09-06 17:44:53 +0000

    Automatically revert commits from previous weekly release
    Change-Id: I4085db7f5528e4256befd0e2e2372088d24e294b
    Signed-off-by: buildslave <buildslave@buildbot.tl.intel.com>

--- common.mk
+++ common.mk
@@ -25,6 +25,10 @@ PRODUCT_PACKAGES += \
     SpeechRecorder
 endif
 
+#Add ccf App
+PRODUCT_PACKAGES += \
+    CC3
+
 # copy boot animation resources
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/bootanimation.zip:system/media/bootanimation.zip
@@ -144,5 +148,3 @@ ifneq (, $(findstring "$(TARGET_BUILD_VARIANT)", "eng" "userdebug"))
      $(COMMON_PATH)/init.dump.rc:root/init.dump.rc
 endif
 
-# AMTL unified configuration
--include vendor/intel/tools/log_capture/amtl/src/com/intel/amtl/config_catalog/amtl.mk
