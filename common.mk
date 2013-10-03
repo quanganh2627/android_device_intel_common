9f0a4e99f313f56b7e904ab5f5ff4410139edc02
Jeevaka Badrappan <jeevaka.badrappan@intel.com>
2013-10-03 12:53:11 +0300

    common: remove ro.spid.telephony.rxdiv
    BZ: 116283
    
    RxDiversity confiuguration is moved to streamline
    configuration files. ro.spid.telephony.rxdiv is not
    used any more.
    
    Change-Id: Ieed6d58185fbf5e008384441ea05a2c9c1200dfd
    Signed-off-by: Jeevaka Badrappan <jeevaka.badrappan@intel.com>
    Reviewed-on: http://android.intel.com:8080/137613
    Reviewed-by: Seibel, Eric <eric.seibel@intel.com>
    Reviewed-by: Ulmer, Lionel <lionel.ulmer@intel.com>
    Reviewed-by: Ravindran, Arun <arun.ravindran@intel.com>
    Reviewed-by: Lucas, Guillaume <guillaume.lucas@intel.com>
    Tested-by: Louis, NicolasX <nicolasx.louis@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- common.mk
+++ common.mk
@@ -79,7 +79,6 @@ PRODUCT_COPY_FILES += \
         $(COMMON_PATH)/init.rc:root/init.rc \
         $(COMMON_PATH)/init.common.rc:root/init.common.rc \
         $(COMMON_PATH)/props.rc:root/props.rc \
-        $(COMMON_PATH)/props.default.rc:root/props.default.rc \
         $(COMMON_PATH)/ueventd.modules.blacklist:root/ueventd.modules.blacklist
 
 PERMISSIONS_PATH := frameworks/native/data/etc
