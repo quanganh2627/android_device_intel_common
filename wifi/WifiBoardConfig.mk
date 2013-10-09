e9ec3d13ee4c7016dd79310fdc8751ed7b6b2aad
Jeremie GARCIA <jeremie.garcia@intel.com>
2013-10-09 10:00:58 +0200

    wifi: make wpa_cli/supplicant and hostapd part of wifi_common package
    BZ: 143777
    
    All products require wpa_supplicant, hostapd and wpa_cli ingredients.
    The "supplicant" source code to use is set by the flag
    WPA_SUPPLICANT_VERSION as the AOSP does.
    
    Category: device enablement
    Domain: CWS.WIFI-Common
    Origin: internal
    Upstream-Candidate: no, not-aosp
    
    Change-Id: Ia2358715d6d2cf51870de11aa94aa997770bf121
    Signed-off-by: Jeremie GARCIA <jeremie.garcia@intel.com>
    Reviewed-on: http://android.intel.com:8080/137312
    Reviewed-by: De Cesco, Jonathan <jonathan.de.cesco@intel.com>
    Reviewed-by: Trivelly, Jean <jean.trivelly@intel.com>
    Reviewed-by: Seibel, Eric <eric.seibel@intel.com>
    Tested-by: Champciaux, Nicolas <nicolas.champciaux@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- wifi/WifiBoardConfig.mk
+++ wifi/WifiBoardConfig.mk
@@ -5,12 +5,13 @@ include $(COMMON)/ComboChipVendor.mk
 BOARD_WPA_SUPPLICANT_DRIVER    := NL80211
 
 ifeq (ti,$(findstring ti,$(COMBO_CHIP_VENDOR)))
+WPA_SUPPLICANT_VERSION := VER_0_8_X
 BOARD_WLAN_DEVICE := wl12xx-compat
 endif
 
 ifeq (bcm,$(findstring bcm,$(COMBO_CHIP_VENDOR)))
 BOARD_WLAN_DEVICE := bcmdhd
-
+WPA_SUPPLICANT_VERSION := VER_0_8_X
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
 BOARD_HOSTAPD_DRIVER        := NL80211
 BOARD_HOSTAPD_PRIVATE_LIB   := lib_driver_cmd_bcmdhd
@@ -40,4 +41,6 @@ endif
 ifeq (intel,$(findstring intel,$(COMBO_CHIP_VENDOR)))
 BOARD_WLAN_DEVICE     := iwlwifi
 BOARD_HOSTAPD_DRIVER  := NL80211
+#WPA_SUPPLICANT_VERSION := VER_2_1_DEVEL_WCS
+WPA_SUPPLICANT_VERSION := VER_0_8_X
 endif
