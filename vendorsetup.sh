140da370321c885c6e8a117235e633f877dc66f8
Hans Odeberg <hans.odeberg@intel.com>
2013-11-20 15:44:24 +0100

    Configure GUI frontends for gdb
    This patch adds two debugging commands, similar to the existing
    command gdbclient_eth.
    
    To open a ddd window connected to a gdbserver on the target:
    gdbclient_ddd ip_addr executable_name [port] [process]
    
    To open an emacs window connected to a gdbserver on the target:
    gdbclient_emacs ip_addr executable_name [port] [process]
    
    Issue: APDEV-545
    
    Change-Id: I138c93611b26b3a46ed4c41278e92bd119576383
    Signed-off-by: Hans Odeberg <hans.odeberg@intel.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -109,3 +109,156 @@ function gdbclient_eth()
 
 }
 
+# Creates a gdb startup file which sets search paths
+# and connects to the target
+function create_gdb_startup_file()
+{
+   local OUT_SYMBOLS=$(get_abs_build_var TARGET_OUT_UNSTRIPPED)
+   local OUT_SO_SYMBOLS=$(get_abs_build_var TARGET_OUT_SHARED_LIBRARIES_UNSTRIPPED)
+
+   echo >|"$OUT_ROOT/gdbclient.cmds" "set solib-absolute-prefix $OUT_SYMBOLS"
+   echo >>"$OUT_ROOT/gdbclient.cmds" "set solib-search-path $OUT_SO_SYMBOLS"
+   echo >>"$OUT_ROOT/gdbclient.cmds" "target remote $IPADDR$PORT"
+   echo >>"$OUT_ROOT/gdbclient.cmds" ""
+}
+
+
+# Yet another gdb debug version, now using a GUI frontend (ddd) over Ethernet
+# $1 : remote IP address (e.g. 192.168.42.1)
+# $2 : executable name (e.g. mediaserver)
+# $3 : port number     (e.g. :5678) - optional
+# $4 : process name    (e.g. mediaserver)
+function gdbclient_ddd()
+{
+   local OUT_ROOT=$(get_abs_build_var PRODUCT_OUT)
+   local OUT_EXE_SYMBOLS=$(get_abs_build_var TARGET_OUT_EXECUTABLES_UNSTRIPPED)
+   local PREBUILTS=$(get_abs_build_var ANDROID_PREBUILTS)
+   if [ "$OUT_ROOT" -a "$PREBUILTS" ]; then
+       local IPADDR="$1"
+       if [ -z "$IPADDR" ]; then
+           echo "Must specify remote IP address"
+           return 1
+       fi
+
+       local EXE="$2"
+       if [ "$EXE" ] ; then
+           EXE=$2
+       else
+           EXE="app_process"
+       fi
+       if [ ! -f "$OUT_EXE_SYMBOLS/$EXE" ]; then
+           echo "Unable to find executable: $OUT_EXE_SYMBOLS/$EXE"
+           return 1
+       fi
+
+       local PORT="$3"
+       if [ "$PORT" ] ; then
+           PORT=$3
+       else
+           PORT=":5039"
+       fi
+
+       local PID
+       local PROG="$4"
+       if [ "$PROG" ] ; then
+           if [[ "$PROG" =~ ^[0-9]+$ ]] ; then
+               PID="$4"
+           else
+               PID=`pid $4`
+           fi
+           adb shell gdbserver $PORT --attach $PID &
+           sleep 2
+       else
+               echo ""
+               echo "If you haven't done so already, do this first on the device:"
+               echo "    gdbserver $PORT /system/bin/$EXE"
+                   echo " or"
+               echo "    gdbserver $PORT --attach $PID"
+               echo ""
+       fi
+
+       create_gdb_startup_file
+
+       if [ "$(get_build_var TARGET_ARCH)" = "x86" ]; then
+           GDB=i686-linux-android-gdb
+       else
+           GDB=arm-linux-androideabi-gdb
+       fi
+       ddd  --gdb --debugger "$GDB"  --command="$OUT_ROOT/gdbclient.cmds" "$OUT_EXE_SYMBOLS/$EXE" 
+
+  else
+       echo "Unable to determine build system output dir."
+   fi
+
+}
+
+# Yet another gdb debug version, now using a GUI frontend (emacs) over Ethernet
+# If you prefer the multi-window debug GUI, add (setq gdb-many-windows "1")
+# to your .emacs file.
+# $1 : remote IP address (e.g. 192.168.42.1)
+# $2 : executable name (e.g. mediaserver)
+# $3 : port number     (e.g. :5678) - optional
+# $4 : process name    (e.g. mediaserver)
+function gdbclient_emacs()
+{
+   local OUT_ROOT=$(get_abs_build_var PRODUCT_OUT)
+   local OUT_EXE_SYMBOLS=$(get_abs_build_var TARGET_OUT_EXECUTABLES_UNSTRIPPED)
+   local PREBUILTS=$(get_abs_build_var ANDROID_PREBUILTS)
+
+   if [ "$OUT_ROOT" -a "$PREBUILTS" ]; then
+       local IPADDR="$1"
+       if [ -z "$IPADDR" ]; then
+           echo "Must specify remote IP address"
+           return 1
+       fi
+       local EXE="$2"
+       if [ "$EXE" ] ; then
+           EXE=$2
+       else
+           EXE="app_process"
+       fi
+       if [ ! -f "$OUT_EXE_SYMBOLS/$EXE" ]; then
+           echo "Unable to find executable: $OUT_EXE_SYMBOLS/$EXE"
+           return 1
+       fi
+       local PORT="$3"
+       if [ "$PORT" ] ; then
+           PORT=$3
+       else
+           PORT=":5039"
+       fi
+       local PID
+       local PROG="$4"
+       if [ "$PROG" ] ; then
+           if [[ "$PROG" =~ ^[0-9]+$ ]] ; then
+               PID="$4"
+           else
+               PID=`pid $4`
+           fi
+           adb shell gdbserver $PORT --attach $PID &
+           sleep 2
+       else
+               echo ""
+               echo "If you haven't done so already, do this first on the device:"
+               echo "    gdbserver $PORT /system/bin/$EXE"
+                   echo " or"
+               echo "    gdbserver $PORT --attach $PID"
+               echo ""
+       fi
+
+       create_gdb_startup_file
+
+       if [ "$(get_build_var TARGET_ARCH)" = "x86" ]; then
+           GDB=i686-linux-android-gdb
+       else
+           GDB=arm-linux-androideabi-gdb
+       fi
+
+       echo > "$OUT_ROOT/emacsdebug.el" "(gdb \"$GDB --annotate=3 $OUT_EXE_SYMBOLS/$EXE -x $OUT_ROOT/gdbclient.cmds\")" 
+       emacs --load=$OUT_ROOT/emacsdebug.el
+  else
+       echo "Unable to determine build system output dir."
+   fi
+
+}
+
