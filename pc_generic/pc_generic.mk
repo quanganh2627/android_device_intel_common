816d2fb926ec914597d5036618a96ca33fefff69
Andrew Boie <andrew.p.boie@intel.com>
2013-12-05 09:43:50 -0800

    pc_generic: fix mixin inherits
    We want GMS if available. Storage mixin changed name; just use
    default. Unbreaks the build.
    
    Change-Id: I19e5fea5e28ccb81c2d4e4ced284439dc88ebbce
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

--- pc_generic/pc_generic.mk
+++ pc_generic/pc_generic.mk
@@ -11,11 +11,10 @@ PRODUCT_COPY_FILES += \
         $(LOCAL_PATH)/init.recovery.rc:root/init.recovery.$(TARGET_PRODUCT).rc
 
 $(call inherit-mixin, cpu-arch, x86)
-$(call inherit-mixin, gms, false)
+$(call inherit-mixin, gms, true)
 $(call inherit-mixin, houdini, true)
 $(call inherit-mixin, boot-arch, efi)
 $(call inherit-mixin, graphics, software)
 $(call inherit-mixin, ethernet, static)
-$(call inherit-mixin, storage, no_sdcard)
 $(call inherit-mixin, fastboot, userfastboot)
 
