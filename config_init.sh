111d9116bde1d1bb633849a22efe05d0336f2784
Guillaume Betous <guillaumex.betous@intel.com>
2014-03-06 16:44:29 +0100

    Provide CONFIG_LIST to publish_build script
    BZ: 167715
    
    Creation of config_init service : creation of symbolic link based
    on software configuration (fastboot oem config command).
    
    CONFIG_LIST contains different software configuration. In order to generate
    different flash.xml this list is passed to publish_build.py
    
    Change-Id: I60b8f1623d72c84f9d8516311588bf24df162f35
    Signed-off-by: Guillaume Betous <guillaumex.betous@intel.com>
    Signed-off-by: Laurent FERT <laurent.fert@intel.com>

---
+++ config_init.sh
@@ -0,0 +1,13 @@
+# software configuration related boot actions
+
+CONFIG_PATH=/local_cfg
+PROPS_FILE=init.props
+
+config=`cat /config/local_config`
+ln -s /system/etc/catalog/$config $CONFIG_PATH
+
+# read all FeatureTeam's init.props file
+for f in `find $CONFIG_PATH/ -name $PROPS_FILE`
+do
+    cat $f >> /config.prop
+done
