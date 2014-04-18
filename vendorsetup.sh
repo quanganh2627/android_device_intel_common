c07a022ff9f01172b89b81a71025c2f12c5a56a1
Jeremy Rocher <jeremy.rocher@intel.com>
2014-04-18 12:14:20 +0200

    build: kernel: add support for KGDB
    BZ: 189547
    
    Provide KGDB support to kernel through a diff config file,
    kgdb_diffconfig, and add mkgdb helper function to compile kernel with
    it.
    
    $ mkgdb -j4
    
    Change-Id: I0dd79b6bbf20ea51ba3bc6d8585bca29998f3476
    Signed-off-by: Jeremy Rocher <jeremy.rocher@intel.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -24,11 +24,27 @@ function mbimg
     (\cd "$T" && make ramdisk-nodeps bootimage-nodeps)
 }
 
+# Rebuild kernel with KGDB support and boot.img
+function mkgdb
+{
+    T=$(gettop)
+    if [ ! "$T" ]; then
+        echo "Couldn't locate the top of the tree.  Try setting TOP." >&2
+        return
+    fi
+    echo ===[ Building Kernel with KGDB support ]===
+    (\cd "$T" && touch device/intel/common/kgdb_diffconfig && \
+        make KERNEL_DIFFCONFIG=device/intel/common/kgdb_diffconfig build_kernel-nodeps $*)
+    echo ===[ Generating Ramdisk and ${ANDROID_PRODUCT_OUT}/boot.img]===
+    (\cd "$T" && make ramdisk-nodeps bootimage-nodeps)
+}
+
 # Print a small help
 help_msg='
 Intel Build shortcuts:
 - mbimg -j4:   fast incremental rebuild of kernel/ramdisk/bootimage.
 - mkernel -j4: fast incremental rebuild of kernel
+- mkgdb -j4   fast incremental rebuild of kernel/ramdisk/bootimage with kgdb.
 '
 
 echo
