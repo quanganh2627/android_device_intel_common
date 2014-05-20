ce106202a0808157e1100dbe049c8b77d61e9675
Patrick Combes <patrick.combes@intel.com>
2014-05-20 11:31:25 +0200

    [REVERTME] Wifi: Disable PM until sideband GPIO implementation
    BZ: 195661
    
    load_inteldriver will disable pm_runtime until the sideband GPIO can wakeup
    the platform.
    
    Change-Id: Ic52f7bd1d3688970fa62e791f00a26db2547f3f6
    Signed-off-by: Patrick Combes <patrick.combes@intel.com>

--- wifi/AndroidWifiIntel.mk
+++ wifi/AndroidWifiIntel.mk
@@ -5,6 +5,9 @@ include $(CLEAR_VARS)
 LOCAL_MODULE :=  wifi_intel
 LOCAL_MODULE_TAGS := optional
 
+LOCAL_REQUIRED_MODULES +=  \
+     load_inteldriver
+
 # Wifi user space components
 LOCAL_REQUIRED_MODULES +=  \
     wifi_intel_usc
@@ -14,3 +17,11 @@ LOCAL_REQUIRED_MODULES +=  \
     wifi_common
 
 include $(BUILD_PHONY_PACKAGE)
+
+##################################################
+include $(CLEAR_VARS)
+LOCAL_MODULE := load_inteldriver
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_SRC_FILES := intel_specific/load_inteldriver
+include $(BUILD_PREBUILT)
