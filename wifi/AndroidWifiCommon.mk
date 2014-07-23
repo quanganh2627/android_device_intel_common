3308e64ca956d90221f2f29b33aa4fa4d949a9cb
Laurent FERT <laurent.fert@intel.com>
2014-07-23 10:18:35 +0200

    [LPDK] Disable wifi_wfa that fails to build
    BZ: 213006
    
    Change-Id: Ief306164c2fbe59c0c3edf2b3a09a38fd2258086
    Signed-off-by: Laurent FERT <laurent.fert@intel.com>
    Reviewed-on: https://android.intel.com/221966

--- wifi/AndroidWifiCommon.mk
+++ wifi/AndroidWifiCommon.mk
@@ -22,7 +22,7 @@ LOCAL_REQUIRED_MODULES :=          \
     hostapd.conf
 
 ifneq (,$(filter $(TARGET_BUILD_VARIANT),eng userdebug))
-LOCAL_REQUIRED_MODULES += \
+#LOCAL_REQUIRED_MODULES += \
     wifi_wfa \
     hostapd_cli  \
     wpa_cli
