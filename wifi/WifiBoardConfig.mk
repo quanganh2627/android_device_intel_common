adbe1acfdd37851aba8d557bf5c67075c33d5b7d
Cedric Vamour <cedric.vamour@intel.com>
2014-08-04 16:43:56 +0200

    Wifi: define custom flag for wpa_supplicant.conf location
    BZ: 214330
    
    From L-Dessert, it is required to flag custom location for
    wpa_supplicant.conf to keep scalable handling of supplicant
    in device/intel.
    
    Change-Id: I37f709e1b4b7d1b57dd9c6b2c618c6e3645640dd
    Signed-off-by: Cedric Vamour <cedric.vamour@intel.com>

--- wifi/WifiBoardConfig.mk
+++ wifi/WifiBoardConfig.mk
@@ -1,5 +1,8 @@
 include $(COMMON_PATH)/ComboChipVendor.mk
 
+#Used for custom wpa_supplicant.conf location (device/intel)
+CUSTOM_WPA_SUPPLICANT_CONF := true
+
 BOARD_WPA_SUPPLICANT_DRIVER    := NL80211
 BOARD_HOSTAPD_PRIVATE_LIB      :=
 BOARD_HOSTAPD_DRIVER           := NL80211
