f5d0c03ccbf94479f48f273933d17c42aebbc68f
shuangluox.zhang <shuangluox.zhang@intel.com>
2014-08-07 15:41:18 +0800

    modem_2230: Add define for the SPI IO ctrl command
    Added define for the SPI IO ctrl command in user space,
    this file was automatically generated from a Linux kernel
    header of the same name.
    
    Change-Id: Ib924857e102638ad8066db9e986d539227f8a17c
    Tracked-On: https://jira01.devtools.intel.com/browse/IMINAN-1848
    Signed-off-by: shuangluox.zhang <shuangluox.zhang@intel.com>
    Reviewed-on: https://android.intel.com:443/259968

---
+++ kernel-headers/linux/spi/xmm2230_spi.h
@@ -0,0 +1,26 @@
+/****************************************************************************
+ ****************************************************************************
+ ***
+ ***   This header was automatically generated from a Linux kernel header
+ ***   of the same name, to make information necessary for userspace to
+ ***   call into the kernel available to libc.  It contains only constants,
+ ***   structures, and macros generated from the original header, and thus,
+ ***   contains no copyrightable information.
+ ***
+ ***   To edit the content of this header, modify the corresponding
+ ***   source file (e.g. under external/kernel-headers/original/) then
+ ***   run bionic/libc/kernel/tools/update_all.py
+ ***
+ ***   Any manual change here will be lost the next time this script will
+ ***   be run. You've been warned!
+ ***
+ ****************************************************************************
+ ****************************************************************************/
+#ifndef _MDM_SPI_IOCTRL_H
+#define _MDM_SPI_IOCTRL_H
+#define SPI_IOCTRL_MAGIC 0x78
+#define SPI_IOC_FRAME_LEN_DOWNLOAD _IO(SPI_IOCTRL_MAGIC, 1)
+/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+#define SPI_IOC_FRAME_LEN_NORMAL _IO(SPI_IOCTRL_MAGIC, 2)
+#endif
+
