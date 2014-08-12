6d50352623092b2bc7e049eb92a971fcd3781558
Jeremy Rocher <jeremy.rocher@intel.com>
2014-08-12 16:43:21 +0200

    logs: update services as kernel message are redirected to 'main' buffer
    BZ: 216411
    
    Kernel message are now redirected to 'main' logger buffer, thus
    'kernel' buffer no longer exists and should be removed from apk_logfs
    service.
    
    Also, filter 'KERNEL' messages from main buffer in other logs
    services.
    
    Change-Id: I606a96e55ec58cf72cec4694ff9413af751c68c5
    Signed-off-by: Jeremy Rocher <jeremy.rocher@intel.com>

--- retrieve_aplogs.sh
+++ retrieve_aplogs.sh
@@ -3,6 +3,6 @@
 prop=`getprop "persist.service.apklogfs.enable"`
 if [ "$prop" -eq 1 ]
 then
-    /sbin/logcat -b system -b events -b main -b radio -b kernel -n 20 -r5000 -v threadtime -d -f /logs/aplog
+    /sbin/logcat -b system -b events -b main -b radio -n 20 -r5000 -v threadtime -d -f /logs/aplog
 fi
 setprop "sys.realpowerctl" "1"
