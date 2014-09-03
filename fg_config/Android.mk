8b846e85f3eb526f6f1a963fcfa42c5664b3813d
Albin B <albin.bala.krishnan@intel.com>
2014-09-03 12:57:29 +0530

    [EM/FG]: Remove the fg_config app from common config directory
    BZ: 221010
    
    Issue Description:
    The Fuel Gauge config app is present under the common config directory.
    
    Solution:
    As the fg_config app is part of energy management module, removed from common
    config directory.
    
    Change-Id: Id3bb7da6c71c43b598757f3ff34dc4cb34aeb5a3
    Category: device enablement
    Domain: BatteryManagement-Common
    Origin: internal
    Upstream-Candidate: no, proprietary
    Signed-off-by: Albin B <albin.bala.krishnan@intel.com>
    Reviewed-on: https://android.intel.com:443/239003

--- fg_config/Android.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH:= $(call my-dir)
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES:= fg_conf_helper.c
-LOCAL_MODULE:= fg_conf
-LOCAL_MODULE_TAGS:= optional
-LOCAL_SHARED_LIBRARIES:= libcutils
-include $(BUILD_EXECUTABLE)
-
