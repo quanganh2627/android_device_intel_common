f036d6eaf54a11e87d52eeb9a141377906122876
Jocelyn Falempe <jocelyn.falempe@intel.com>
2014-10-21 14:07:13 +0200

    Add mbimg and mkernel bash alias
    mbimg allows to quickly rebuild the kernel and a bootimage.
    mkernel allows to quickly build kernel specific target like :
    menuconfig/xconfig/gconfig or tags/cscope ...
    
    usage is :
    
    mbimg -j8
    
    mkernel xconfig
    
    Change-Id: Id393d3d6df31138aa5c33924222ca15efa4e8917
    Tracked-On: https://jira01.devtools.intel.com/browse/IMINAN-8347
    Signed-off-by: Jocelyn Falempe <jocelyn.falempe@intel.com>
    Reviewed-on: https://android.intel.com:443/297854

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,3 +8,15 @@ if [ -f device/intel/mixins/mixin-update ]; then
         echo '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
     fi
 fi
+
+mbimg() {
+    local T=$(gettop)
+    ONE_SHOT_MAKEFILE=device/intel/mixins/groups/kernel/kernel-src.mk make -C $T -f build/core/main.mk kernel || return
+    echo ===[ Generating Ramdisk and ${ANDROID_PRODUCT_OUT}/boot.img]===
+    (\cd "$T" && make kernel ramdisk-nodeps bootimage-nodeps)
+}
+
+mkernel() {
+    local T=$(gettop)
+    ONE_SHOT_MAKEFILE=device/intel/mixins/groups/kernel/kernel-src.mk make -C $T -f build/core/main.mk BUILD_KERNEL_FROM_SOURCES=1 $*
+}
