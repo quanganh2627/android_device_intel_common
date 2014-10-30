4f45bf7e640292ec4d4cb655514e022987963ec4
Fabien Marotte <fabien.marotte@intel.com>
2014-10-30 18:13:20 +0100

    Optimize dex at compilation time
    This improves the first boot time
    
    Change-Id: I54b523db94ce3c1d127cca5df21e2682406e45fd
    Tracked-On: https://jira01.devtools.intel.com/browse/IMINAN-7477
    Signed-off-by: Fabien Marotte <fabien.marotte@intel.com>
    Reviewed-on: https://android.intel.com:443/295067

--- base_x86_legacy/BoardConfig.mk
+++ base_x86_legacy/BoardConfig.mk
@@ -188,10 +188,7 @@ BOARD_SEPOLICY_UNION := \
 ##############################################################
 # Enable dex-preoptimization to speed up the first boot sequence
 # Note that this operation only works on Linux for now
-# Enable for user and eng builds
-ifneq ($(TARGET_BUILD_VARIANT),userdebug)
 WITH_DEXPREOPT := true
-endif
 
 ##############################################################
 # Source: device/intel/mixins/groups/mixin-check/default/BoardConfig.mk
