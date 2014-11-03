29f63fd2c6ea2e8f32b7836b6c9001e46e874c34
Nitheesh K L <nitheesh.k.l@intel.com>
2014-11-02 22:25:40 -0800

    [PORT] The memory settings should be adjusted for 1G memory devices
    Issue: IMINAN-3856
    
    It is a port of the patch for the issue IMINAN-3856 to IMIN WS from GMIN.
    The setting for devices were provided by Sridhar, Kanchana.
    
    Change-Id: Ide761f670329360d8500755b5f2374c3d595fcc1
    Tracked-On: https://jira01.devtools.intel.com/browse/IMINAN-3856
    Signed-off-by: Nitheesh K L <nitheesh.k.l@intel.com>
    Reviewed-on: https://android.intel.com:443/295896

--- dalvik/tablet-hdpi-1024-dalvik-heap.mk
+++ dalvik/tablet-hdpi-1024-dalvik-heap.mk
@@ -1,8 +1,7 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapstartsize=8m \
-    dalvik.vm.heapgrowthlimit=128m \
+    dalvik.vm.heapgrowthlimit=100m \
     dalvik.vm.heapsize=174m \
     dalvik.vm.heaptargetutilization=0.75 \
     dalvik.vm.heapminfree=512k \
-    dalvik.vm.heapmaxfree=8m \
-    dalvik.jit.code_cache_size=1048576
+    dalvik.vm.heapmaxfree=8m
