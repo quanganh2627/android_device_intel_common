617a674104ce24597c121f4c700c335befc0b901
Laurent FERT <laurent.fert@intel.com>
2014-11-07 18:53:05 +0100

    Split config selection and setprops
    Config selection must be done synchronously to ensure rootfs is not
    mounted r/o before some files are created in the rootfs.
    
    Setting properties can be done asynchronously hence avoiding a deadlock
    with init not processing setprop messages while it is stuck in the wait
    command.
    
    Change-Id: I9dcf3759c55f4ca14114aabea40430aa239347f4
    Tracked-On: https://jira01.devtools.intel.com/browse/IMINAN-8422
    Signed-off-by: Laurent FERT <laurent.fert@intel.com>
    Reviewed-on: https://android.intel.com:443/298559

--- config_init.sh
+++ config_init.sh
@@ -1,29 +1,10 @@
-# software configuration related boot actions
-
-CONFIG_PATH=/local_cfg
-PROPS_FILE=init.props
+# Get selected software configuration
 
 config=`cat /config/local_config`
-ln -s /system/etc/catalog/$config $CONFIG_PATH
+ln -s /system/etc/catalog/$config /local_cfg
 
 log -p i -t config_init "Activating configuration $config"
 
-# read all FeatureTeam's init.props file
-for f in $CONFIG_PATH/*/$PROPS_FILE
-do
-    while read l; do
-
-        # Ignore empty lines and comments
-        case "$l" in
-            ''|'#'*)
-                continue
-                ;;
-        esac
-
-        # Set property
-        setprop `echo ${l/=/ }`
-
-    done < $f
-done
-
+# Boot flow has to be suspended so that the ramdisk is not re-mounted r/o.
+# Signal configuration is done so that boot flow can resume.
 echo > /config_init.done
