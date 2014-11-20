67e3235e1755e9458885e3494cfc105ab417cdd6
Jeremy Rocher <jeremy.rocher@intel.com>
2014-11-20 16:11:20 +0100

    vendorsetup.sh: mbimg,mkernel: use kernel makefile from target
    Current version use kernel makefile from kernel mixin group, this is
    not working as some values are not set properly.
    
    Instead use AndroidBoard.mk from selected lunch target.
    
    Change-Id: Ia56069655759ef0c943392ed97532495dfa47051
    Tracked-On: https://jira01.devtools.intel.com/browse/IMINAN-9925
    Signed-off-by: Jeremy Rocher <jeremy.rocher@intel.com>
    Reviewed-on: https://android.intel.com:443/303477

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,14 +7,22 @@ if [ -f device/intel/mixins/mixin-update ]; then
     fi
 fi
 
-mbimg() {
-    local T=$(gettop)
-    ONE_SHOT_MAKEFILE=device/intel/mixins/groups/kernel/kernel-src.mk make -C $T -f build/core/main.mk kernel || return
-    echo ===[ Generating Ramdisk and ${ANDROID_PRODUCT_OUT}/boot.img]===
-    (\cd "$T" && make kernel ramdisk-nodeps bootimage-nodeps)
-}
-
 mkernel() {
     local T=$(gettop)
-    ONE_SHOT_MAKEFILE=device/intel/mixins/groups/kernel/kernel-src.mk make -C $T -f build/core/main.mk BUILD_KERNEL_FROM_SOURCES=1 $*
+    local KERNEL_MAKEFILE=$(\cd "$T" && find device -name ${TARGET_PRODUCT}.mk | sed -e "s/${TARGET_PRODUCT}\.mk/AndroidBoard\.mk/")
+
+    if [ ! -f $T/$KERNEL_MAKEFILE ] ; then
+        echo "Kernel makefile not found for TARGET_PRODUCT=$TARGET_PRODUCT. abort" 1>&2
+        return 1
+    fi
+    ONE_SHOT_MAKEFILE=$KERNEL_MAKEFILE make -C $T -f build/core/main.mk BUILD_KERNEL_FROM_SOURCES=1 $*
+}
+
+mbimg() {
+    local T=$(gettop)
+
+    mkernel kernel || return
+
+    echo ===[ Generating Ramdisk and ${ANDROID_PRODUCT_OUT}/boot.img]===
+    (\cd "$T" && make BUILD_KERNEL_FROM_SOURCES=1 ramdisk-nodeps bootimage-nodeps)
 }
